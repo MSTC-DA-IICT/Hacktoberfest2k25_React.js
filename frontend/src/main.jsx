fd6425bd55dccce09af65e9b7d60611388f64e6a
Dhruvish-Tandel <dhruvishtandel12@gmail.com>
2025-10-15 01:33:00 +0530

    Updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,19 +14,7 @@
  * 7. Add Toaster component for toast notifications
  * 8. Render to root element
  *
- * STRUCTURE:
- * <BrowserRouter>
- *   <App />
- *   <Toaster />
- * </BrowserRouter>
- */
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import App from './App.jsx';
-import './index.css';
+ 
 
 /**
  * TODO: RENDER APPLICATION
@@ -48,34 +36,3 @@ import './index.css';
  * );
  */
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          duration: 3000,
-          style: {
-            background: '#363636',
-            color: '#fff',
-          },
-          success: {
-            duration: 3000,
-            iconTheme: {
-              primary: '#4ade80',
-              secondary: '#fff',
-            },
-          },
-          error: {
-            duration: 4000,
-            iconTheme: {
-              primary: '#ef4444',
-              secondary: '#fff',
-            },
-          },
-        }}
-      />
-    </BrowserRouter>
-  </React.StrictMode>
-);
